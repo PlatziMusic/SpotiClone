5b44d5571c210c38c2910f6f0c5ff9f915f7e5b9
luiscolungaperez <perez.luis.colunga@gmail.com>
2020-07-08 17:13:38 -0500

    Add styles

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import "../assets/styles/components/Register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  const firstContainer = useRef();
+  const secondContainer = useRef();
+
+  const handleNextContainer = () => {
+    firstContainer.current.style.display = "none";
+    secondContainer.current.style.display = "block";
+  };
+
+  const handlePreviusContainer = () => {
+    firstContainer.current.style.display = "block";
+    secondContainer.current.style.display = "none";
+  };
+
+  return (
+    <section className="Container">
+      <section className="firstContainer" ref={firstContainer}>
+        <p>Hola mundo</p>
+        <p>Como estas</p>
+        <button type="button" onClick={handleNextContainer}>
+          Next
+        </button>
+      </section>
+      <section className="secondContainer" ref={secondContainer}>
+        <button type="button" onClick={handlePreviusContainer}>
+          Next
+        </button>
+      </section>
+    </section>
+  );
 };
 
 export default Register;
