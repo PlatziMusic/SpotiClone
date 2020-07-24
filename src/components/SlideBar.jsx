e359ab368eea3e31b6971fe3a152cbd0b73397a2
Luis Alberto colunga Perez <perez.luis.colunga@gmail.com>
2020-07-24 13:49:35 -0500

    fixed eslint rules

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -1,116 +1,116 @@
-import React from "react";
-import "../assets/styles/components/SlideBar.scss";
-import logoSpoti from "../assets/static/logo.png";
-import HomeIcon from "@material-ui/icons/Home";
-import SearchIcon from "@material-ui/icons/Search";
-import QueueMusicIcon from "@material-ui/icons/QueueMusic";
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../assets/styles/components/SlideBar.scss';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import QueueMusicIcon from '@material-ui/icons/QueueMusic';
+import { Link } from 'react-router-dom';
+import logoSpoti from '../assets/static/logo.png';
 
 const SlideBar = () => {
   return (
-    <section className="slidebar-container">
-      <div className="logo">
-        <img src={logoSpoti} alt="Logo" />
+    <section className='slidebar-container'>
+      <div className='logo'>
+        <img src={logoSpoti} alt='Logo' />
       </div>
 
-      <div className="slidebar-menu">
-        <div className="slidebar-menu-option">
-          <Link to="/Home">
-            <HomeIcon className="icon-menu" />
+      <div className='slidebar-menu'>
+        <div className='slidebar-menu-option'>
+          <Link to='/Home'>
+            <HomeIcon className='icon-menu' />
             <p>Inicio</p>
           </Link>
         </div>
-        <div className="slidebar-menu-option">
-          <Link to="/">
-            <SearchIcon className="icon-menu" />
+        <div className='slidebar-menu-option'>
+          <Link to='/'>
+            <SearchIcon className='icon-menu' />
             <p>Buscar</p>
           </Link>
         </div>
-        <div className="slidebar-menu-option">
-          <Link to="/">
-            <QueueMusicIcon className="icon-menu" />
+        <div className='slidebar-menu-option'>
+          <Link to='/'>
+            <QueueMusicIcon className='icon-menu' />
             <p>Tu Biblioteca</p>
           </Link>
         </div>
       </div>
 
-      <div className="slidebar-newPlaylist">
-        <button type="button">
-          <Link to="/">Crear una playlist</Link>
+      <div className='slidebar-newPlaylist'>
+        <button type='button'>
+          <Link to='/'>Crear una playlist</Link>
         </button>
       </div>
 
-      <div className="slidebar-yourPlaylist">
+      <div className='slidebar-yourPlaylist'>
         <h2>Tus playlist</h2>
-        <div className="slidebar-yourPlaylist-list">
+        <div className='slidebar-yourPlaylist-list'>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
           <p>
-            <Link to="/Playlist">Lista 1</Link>
+            <Link to='/Playlist'>Lista 1</Link>
           </p>
         </div>
       </div>
