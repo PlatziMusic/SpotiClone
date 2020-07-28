bb8f97cdd20a0168c591e28ddb5c8facb277833c
Luis Alberto colunga Perez <perez.luis.colunga@gmail.com>
2020-07-27 19:27:55 -0500

    update architecture

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,31 @@
-import React, { useRef } from "react";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import "../assets/styles/components/Card.scss";
+import React, { useRef } from 'react';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import '../assets/styles/components/Card.scss';
 
 const Card = ({ title, info, image }) => {
   const btn = useRef();
 
   return (
     <section
-      className="card"
+      className='card'
       onMouseEnter={() => {
-        btn.current.className = "active";
+        btn.current.className = 'active';
       }}
       onMouseLeave={() => {
         btn.current.className = null;
       }}
     >
-      <section className="card__image">
-        <img src={image} alt="Imagen" />
+      <section className='card__image'>
+        <img src={image} alt='Imagen' />
       </section>
-      <section className="card__info">
-        <div className="card__info--data">
+      <section className='card__info'>
+        <div className='card__info--data'>
           <h2>{title}</h2>
           <p>{info}</p>
         </div>
-        <div className="card__info--btn">
-          <button ref={btn}>
-            <PlayArrowIcon className="PlayArrowIcon" />
+        <div className='card__info--btn'>
+          <button type='button' ref={btn}>
+            <PlayArrowIcon className='PlayArrowIcon' />
           </button>
         </div>
       </section>
