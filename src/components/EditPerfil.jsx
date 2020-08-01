59cd7f6bf83c2ffd83c9247a8c9574d9b9e80e07
Max23Esau <esa-max23@live.com>
2020-08-01 03:25:20 -0500

    add view Perfil and EditPerfil

---
+++ src/components/EditPerfil.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../assets/styles/components/Perfil.scss';
+
+const EditPerfil = () => {
+  return (
+    <section>
+      <header className='playlistHeader'>
+        <img src='' alt='Logo' />
+      </header>
+      <main className='editPlaylistMain'>
+        <article className='editPlaylistData'>
+          <h3>Editar Perfil</h3>
+          <form action='' className='form'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' id='' />
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' name='password' id='' />
+            <div className='birthday'></div>
+          </form>
+          <button type='button' className='btn'>Cancelar</button>
+          <button type='button' className='btn'>Guardar perfil</button>
+        </article>
+      </main>
+    </section>
+  );
+};
+
+export default EditPerfil;
---
+++ src/components/Perfil.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../assets/styles/components/Perfil.scss';
+
+const Perfil = () => {
+  return (
+    <section>
+      <header className='playlistHeader'>
+        <img src='' alt='Logo' />
+        <button type='button' className='btn'>Regresar al reproductor</button>
+      </header>
+      <main className='playlistMain'>
+        <h1 className='playlistMain-title'>Vista general de la cuenta</h1>
+        <article className='playlistData'>
+          <h3>Perfil</h3>
+          <table className='playlistData-table'>
+            <tbody className='tbody'>
+              <tr className='tr'>
+                <td>Nombre de usuario</td>
+                <td>Juan Perez</td>
+              </tr>
+              <tr className='tr'>
+                <td>Email</td>
+                <td>JuanPerez@gmail.com</td>
+              </tr>
+              <tr className='tr'>
+                <td>Fecha de Nacimiento</td>
+                <td>25 de Febrero del 1995</td>
+              </tr>
+              <tr className='tr'>
+                <td>País</td>
+                <td>México</td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
+        <button type='button' className='btn'>Editar perfil</button>
+      </main>
+    </section>
+  );
+};
+
+export default Perfil;
