b8b997059020664bb5a0277aa8a16d6b2458067c
Max23Esau <esa-max23@live.com>
2020-08-22 22:01:08 -0500

    add media queries

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -6,7 +6,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import QueueMusicIcon from '@material-ui/icons/QueueMusic';
 
 import NewPlaylist from './NewPlaylist';
-import logoSpoti from '../assets/static/logo.png';
+// import logoSpoti from '../assets/static/logo.png';
 import '../assets/styles/components/SlideBar.scss';
 
 const SlideBar = () => {
@@ -15,9 +15,9 @@ const SlideBar = () => {
   });
   return (
     <section className='slidebar-container'>
-      <div className='logo'>
+      {/* <div className='logo'>
         <img src={logoSpoti} alt='Logo' />
-      </div>
+      </div> */}
 
       <div className='slidebar-menu'>
         <div className='slidebar-menu-option'>
